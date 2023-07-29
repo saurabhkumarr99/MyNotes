dce5bec6cf81e68c47c6045c0a28dda581dddb72
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2023-07-29 15:34:29 +0530

    java compile and run command

--- scripts/CommonJS.js
+++ scripts/CommonJS.js
@@ -13,3 +13,18 @@ function prepareFrame(position, src) {
     document.getElementById(position).style.display="block";
     document.getElementById(position).appendChild(ifrm);
 }
+
+function prepareFrame2(position) {
+    let cmds=`<h2>Compile and run java program</h2><br>
+              <p>Step 1- Create a java program with .java extension</p> <br>
+              <p>Step 2- compile the program<br>
+              <code>javac HelloWorld.java</code></p><br>
+              <p>Step 3-Run the class file<br>
+              <code>java abc <br> java HelloWorld.java</code></p><br>
+              <p>Step 4-Run java jar file<br>
+              <code>java -jar HelloWorld.jar</code></p>`;
+
+    document.getElementById(position).innerHTML = "";
+    document.getElementById(position).style.display="block";
+    document.getElementById(position).innerHTML = cmds;
+}
